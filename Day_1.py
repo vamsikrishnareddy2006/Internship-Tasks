b441a97f923d0bf1fb9108b543f3ff6266e0bd80
Vamsi Krishna Reddy <vamsikrishnareddyvemireddy2006@gmail.com>
2026-07-02 14:53:33 +0530

    initial commit

---
+++ Day_1.py
@@ -0,0 +1,17 @@
+# Define your profile variables
+name = "V.Vamsi Kr5ishna Reddy"
+age = 20
+learning = "Python"
+has_coded = True
+goal = "Full Stack AI Developer"
+
+# Print the formatted output
+print("=========================================")
+print("        MY DEVELOPER PROFILE CARD")
+print("=========================================")
+print(f"Name              : {name}")
+print(f"Age               : {age}")
+print(f"Learning          : {learning}")
+print(f"Has Coded Before? : {has_coded}")
+print(f"Goal              : {goal}")
+print("=========================================")
